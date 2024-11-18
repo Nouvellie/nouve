efca91ee84683ecee15fb950d1828bbe28a15ceb
roberto <rr.litost@gmail.com>
2024-11-18 18:47:00 -0300

    v0.0.3

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -12,4 +12,6 @@ urlpatterns = [
     path('api/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify', TokenVerifyView.as_view(), name='token_verify'),
+
+    path('api/users/', include('users.urls')),
 ]
